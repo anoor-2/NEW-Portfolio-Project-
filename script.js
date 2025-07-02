6c8c54eeb70b96516a926d481ae600713002756c
anoor1 <averinoor2020@gmail.com>
2025-07-01 20:42:49 -0400

    case study

--- script.js
+++ script.js
@@ -3,3 +3,19 @@
 //  homeBtn.addEventListener('click',function(){
 
 //   });
+
+document.getElementById('unit2p').onclick=function(){
+    window.open("https://anoor-2.github.io/int-u2-project-starter-code-3/", "_blank");
+}
+
+document.getElementById('unit3p').onclick=function(){
+    window.open("https://anoor-2.github.io/int-u3-project-starter-code/", "_blank");
+}
+
+document.getElementById('unit4p').onclick=function(){
+    window.open("https://anoor-2.github.io/int-u4-project-23-24-starter-code/", "_blank");
+}
+
+document.getElementById('unit4p').onclick=function(){
+    window.open("http://127.0.0.1:3004/index.html", "_blank");
+}
