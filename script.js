a63f129fd08c1d09346c0f8046cce09107a8ec0b
anoor1 <averinoor2020@gmail.com>
2025-07-01 22:31:40 -0400

    code

--- script.js
+++ script.js
@@ -16,6 +16,6 @@ document.getElementById('unit4p').onclick=function(){
     window.open("https://anoor-2.github.io/int-u4-project-23-24-starter-code/", "_blank");
 }
 
-document.getElementById('unit4p').onclick=function(){
+document.getElementById('speedType').onclick=function(){
     window.open("http://127.0.0.1:3004/index.html", "_blank");
 }
